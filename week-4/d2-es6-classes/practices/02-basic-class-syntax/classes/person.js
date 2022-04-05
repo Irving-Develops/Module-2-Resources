cfc13f71226861ae14109bb1a78c2d118ecb7a18
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-05 12:25:54 -0500

    person class

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,4 +1,43 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(people) {
+
+    if (!Array.isArray(people)) {
+      console.log("introducePeople only takes an array as an argument.")
+      return;
+    }
+    for (let person of people) {
+      if (!(person instanceof Person)) {
+        console.log("All items in the array must be Person class instances.");
+        return;
+      }
+    }
+    people.forEach(person => person.introduce());
+  }
+}
+
+const Irving = new Person(
+  'Irving',
+  'Arreola',
+  23);
+
+const Cris = new Person(
+  'Cris',
+  'Arreola',
+  21);
+
+  
+console.log(Person.people)
+console.log(Person.introducePeople([Irving, Cris]));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
