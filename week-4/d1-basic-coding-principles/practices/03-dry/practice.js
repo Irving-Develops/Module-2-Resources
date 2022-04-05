49ea93217509d481789f9d9ee39658936d90522f
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-04 20:39:32 -0500

    in class problems

--- week-4/d1-basic-coding-principles/practices/03-dry/practice.js
+++ week-4/d1-basic-coding-principles/practices/03-dry/practice.js
@@ -1,38 +1,28 @@
+function biggerNumber(num1, num2){
+  if(num2 > num1) return num2;
+  return num1;
+}
+
+
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  let biggerNum = biggerNumber(num1, num2);
+  return biggerNum * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
+  let bigNum = biggerNumber(num1, num2);
     return bigNum / 3;
   }
-}
+
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  let bigNum = biggerNumber(sum1, sum2);
+  return `I ate ${bigNum} tacos.`;
+  
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
+  let smallDog = biggerNumber(weight1, we);
   if (weight1 < weight2) {
     smallDog = weight1;
     return `I adopted a dog that weighs ${smallDog} pounds.`;
