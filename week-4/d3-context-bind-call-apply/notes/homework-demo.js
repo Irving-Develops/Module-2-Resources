d9ac2e63d25b7e7eb19d68f5b82f2ed808f819ba
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-06 10:10:41 -0500

    homework notes

--- week-4/d3-context-bind-call-apply/notes/homework-demo.js
+++ week-4/d3-context-bind-call-apply/notes/homework-demo.js
@@ -38,7 +38,14 @@
 // let cat = new Cat();
 
 // const boundPurr = cat.purrMore.bind(cat);
-// console.log(cat.purrMore.bind(cat));
+
+//will print [function: bound purrMore]
+//console.log(cat.purrMore.bind(cat));
+//console.log(boundPurr);
+
+//will print meeeeooooow
+//console.log(cat.purrMore())
+
 //global.setTimeout(boundPurr, 5000);
 
 
@@ -68,13 +75,18 @@
 //         this.name = name;
 //     }
 
-//     sayName() {
-//         console.log(this.name)
+//     // sayName() {
+//     //     console.log(this.name)
+//     // }
+
+//     purrNTimes(n) {
+//         for(let i=0; i<n; i++) {
+//             console.log(`${this.name} says: meow`)
+//         }
 //     }
 // }
 
-// let cat = new Cat("Scrappy");
-//console.log(cat.sayName())
+// // console.log(cat.sayName())
 
 // class Dog {
 //     constructor(name){
@@ -83,6 +95,12 @@
 // }
 
 // let dog = new Dog("Bella")
+// let cat = new Cat("Scrappy");
+
+// //makes bella meow 5 times
+// //cat.purrNTimes.call(dog, 5);
+
+// cat.purrNTimes.apply(dog, [5]);
 
 // let sayNameFunc = cat.sayName;
 
@@ -94,3 +112,37 @@
 
 // const boundPurr = cat.purrMore.bind(cat);
 
+
+
+//Call and Apply =================================================
+
+//assigning return of func.call() to a variable
+// let callReturn = func.call(context, ...args)
+
+//assigning return of func.apply() to a variable
+// let applyReturn = func.apply(context, [...args]);
+
+
+//the first argument is the this that you want to bind to the functions context
+
+//apply takes in an array of arguments
+
+//call spreads them out in comma separated values
+
+
+
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+
+    bark = () => {
+        console.log(`${this.name} barked at you`);
+    }
+}
+
+const fido = new Dog("Fido");
+fido.bark(); // Fido barked at you
+const fidoBark = fido.bark;
+fidoBark(); //
