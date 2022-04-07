8e341210d73ade9c40660fb3e064e186efc7438b
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-07 12:48:26 -0500

    func.js tests completed

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,39 @@
 function isFive(num) {
   // Your code here
+  if(typeof num !== 'number' || num !== 5){
+    return false;
+  } 
+  return true;
 }
 
 function isOdd(number) {
   // Your code here
+  
+    if(typeof number !== 'number'){
+      throw new Error('Invalid number')
+    }
+  if(number % 2 === 0){
+    return false;
+  }
+  return true;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const newArr = [];
+  if(min > max){
+    return newArr;
+  } else if(!step) {
+    for(let i = min; i <= max; i++){
+      newArr.push(i);
+    }
+    return newArr;
+  } else {
+    for(let i = min; i <= max; i+= step){
+      newArr.push(i)
+    }
+    return newArr;
+  }
 }
 
 
