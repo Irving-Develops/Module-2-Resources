56bfa8c01fd79e87d9d500b8f67afbbfed3e3892
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-11 16:37:34 -0500

    practice 1

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
@@ -3,11 +3,22 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
+  let newArr = [];
+  for (let i = increment; i <= increment * 10; i+= increment) {
+    newArr.push(addNums(i));
+  }
+  return newArr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  newArr = [];
+  for (let i = increment; i <= increment * 10; i += increment) {
+    newArr.push(addManyNums(i));
+  }
+  return newArr;
 }
+console.log(addNums10(10));
 
 module.exports = [addNums10, addManyNums10];
--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -1,22 +1,41 @@
 const [addNums, addManyNums] = require("./phase-1");
-
+// let startTime = new Date();
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
+  console.time("Timer 1", "<-------- Start")
+    let newArr = [];
+    for (let i = increment; i <= increment * 10; i += increment) {
+      newArr.push(addNums(i));
+      console.timeLog("Timer 1")
+    }
+    console.timeEnd("Timer 1", "<-------- End")
+    return newArr;
 }
+// let endTime = new Date()
+// console.log(`Runtime: ${endTime - startTime}ms`);
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
 // Then, add timing code
+  console.time("Timer 2", '<------- Start')
+  newArr = [];
+  for (let i = increment; i <= increment * 10; i += increment) {
+    newArr.push(addManyNums(i));
+    console.timeLog("Timer 2")
+  }
+  console.timeEnd("Timer 2", "<-------- End")
+  return newArr;
 }
-
 
 n = 1000000
 console.log(`addNums(${n}): `);
 addNums10Timing(1000000);
-
 console.log("\n***********\n");
+
+
+
 
 n = 1000
 console.log(`addManyNums(${n}): `);
