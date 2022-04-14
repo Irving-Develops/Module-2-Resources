20a204c9e0090c30372c538fa3da66a216d419f2
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-13 23:46:15 -0500

    hash-table-pt2 complet

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -14,10 +14,7 @@ class HashTable {
     // Your code here
     this.count = 0;
     this.capacity = numBuckets;
-    this.data = [];
-    for(let i = 0; i < numBuckets; i++){
-      this.data[i] = null;
-    }
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
@@ -34,6 +31,14 @@ class HashTable {
 
   insertNoCollisions(key, value) {
     // Your code here
+    let indexOfInsert = this.hashMod(key);
+    let keyValuePair = new KeyValuePair(key, value);
+    //let newKeyValuePair = new KeyValuePair(key, value);
+    if(this.data[indexOfInsert] !== null){
+      throw new Error('hash collision or same key/value pair already exists!')
+    }
+    this.data[indexOfInsert] = keyValuePair;
+    this.count++;
   }
 
   insertWithHashCollisions(key, value) {
@@ -47,13 +52,18 @@ class HashTable {
 }
 
 
-    const hashTable = new HashTable(2);
+    // const hashTable = new HashTable(2);
+    // console.log(hashTable.data, "<------ data");
+    // let  newKV = new KeyValuePair(105, "john")
+    // hashTable.data[0] = newKV;
+    // console.log(hashTable.data, "<------ data");
+    // console.log(newKV)
+    // let code = sha256("C");
+    // // console.log(code)
+    // // code = code.slice(2 , 10)
+    // let num = parseInt(code.slice(0, 8), 16);
+    // // console.log(num);
+    // console.log(hashTable.insertNoCollisions("C"))
 
-    let code = sha256("C");
-    // console.log(code)
-    // code = code.slice(2 , 10)
-    let num = parseInt(code.slice(0, 8), 16);
-    // console.log(num);
-    console.log(hashTable.hashMod("ABC"))
 
 module.exports = HashTable;
