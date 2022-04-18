30053e5d188a36f669ae96e76ad9967e5223379e
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-18 15:47:20 -0500

    bubble sort

--- week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
@@ -1,18 +1,39 @@
-
 function bubbleSort(arr) {
+let swapped = true;;
+  while (swapped) {
+    swapped = false;
 
-  // Iterate through the array
-
-    // If the current value is greater than its neighbor to the right
-      // Swap those values
-
-      // Do not move this console.log
-      console.log(arr.join(","));
-
-  // If you get to the end of the array and no swaps have occurred, return
-
-  // Otherwise, repeat from the beginning
-
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i+1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        
+        swapped = true;
+        console.log(arr.join(","));
+      }
+    }
+    
+  }
+  return arr;
 }
+
+// function bubbleSort(arr) {
+
+//   // Iterate through the array
+//   for (let i = 0; i < arr.length; i++) {
+//     // If the current value is greater than its neighbor to the right
+//     let current = arr[i];
+//     let next = arr[i + 1];
+//     if (current > next) {
+//       // Swap those values
+//       [current, next] = [next, current];
+//       // Do not move this console.log
+//       console.log(arr.join(","));
+//     }
+//     // If you get to the end of the array and no swaps have occurred, return
+//     if ()
+//   }
+//   // Otherwise, repeat from the beginning
+
+// }
 
 module.exports = bubbleSort;
