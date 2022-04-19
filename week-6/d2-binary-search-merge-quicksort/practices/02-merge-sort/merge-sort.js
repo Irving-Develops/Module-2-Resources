77c10a563b93a3d8ad46eee53ce2350edce20243
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-19 09:56:17 -0500

    sorting

--- week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -3,15 +3,21 @@
 function mergeSort(arr) {
 
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
-
+  // If so, it's already sorted: return
+  if(arr.length <= 1) return arr;
   // Divide the array in half
-
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid)
+  console.log(left, "<--- left")
+  console.log(right, "<--- right")
+  return merge(
   // Recursively sort the left half
+  mergeSort(left),
   // Recursively sort the right half
-
+  mergeSort(right)
   // Merge the halves together and return
-
+  );
 }
 
 
@@ -19,15 +25,27 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
-
+  let arr = [];
   // Point to the first value of each array
+  let arrAIndex = 0;
+  let arrBIndex = 0;
   // While there are still values in each array...
+  while(arrA.length > arrAIndex || arrB.length > arrBIndex) {
     // Compare the first values of each array
+    if(arrA[arrAIndex]< arrB[arrBIndex]){
     // Add the smaller value to the return array
+    arr.push(arrA[arrAIndex])
     // Move the pointer to the next value in that array
-
+      arrAIndex++;
+    }else{
+      arr.push(arrB[arrBIndex]);
+      arrBIndex++;
+    }
+  }
+  return arr;
   // Return the return array
 
 }
-
+console.log(merge([1,4,8], [2,3,6,9]))
+console.log(mergeSort([2, 4, 6, 8, 1, 3, 5, 7, 9]))
 module.exports = [merge, mergeSort];
--- week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
@@ -1,10 +1,11 @@
 function quicksort(arr) {
 
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // If so, it's already sorted: return
+  if( arr.length <= 1) return arr;
 
   // Pick the first value as the pivot
-
+  let pivot = arr[0];
   // Orient the pivot so that...
       // every number smaller than the pivot is to the left
       // every number larger (or equal) than the pivot is to the right
