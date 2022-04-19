164de29c7abfc9002df6380e1107f29977755188
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-18 21:12:05 -0500

    3

--- week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
@@ -3,13 +3,13 @@
 function selectionSort(arr) {
 
   // Copy the original array
-
+  // let arrCopy = [...arr]
   // Create an array to store the sorted values
-
+  // let sortedArr = [];
   // While the array is not empty...
-
+    // while(arr)
     // Do not move this console.log
-    console.log(sorted.join(","));
+    // console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
 
@@ -23,12 +23,25 @@ function selectionSort(arr) {
 
 function selectionSortInPlace(arr) {
 
+  for(let i = 0; i < arr.length; i++){
+    let min = i;
+    console.log(arr.join(","));
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[min]){
+        min = j;
+      }
+    }
+    if(i !== min){
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+
   // Set a pointer at zero diving the array into sorted and unsorted halves
 
   // Repeat while the unsorted half is not empty:
 
     // Do not move this console.log
-    console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
 
